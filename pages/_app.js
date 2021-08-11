8a741a0351aec1cae01a6262edcf363579a56bab
rujack <ffggabc123@gmail.com>
2021-08-11 13:34:30 +0700

    pedal v1

--- pages/_app.js
+++ pages/_app.js
@@ -4,11 +4,9 @@ import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "nprogress/nprogress.css";
 import Layout from "../components/Layout";
-<<<<<<< HEAD
 
 function MyApp({ Component, pageProps,router }) {
 
-=======
 // import { useEffect } from "react";
 
 function MyApp({ Component, pageProps,router }) {
@@ -17,7 +15,6 @@ function MyApp({ Component, pageProps,router }) {
 //     Router.push('/maintenance')
 //   },[])
 
->>>>>>> ae2d768897ee385f693f14a95dbe51556f4aa642
   nProgress.configure({ showSpinner: false });
 
   Router.events.on("routeChangeStart", (url) => {
