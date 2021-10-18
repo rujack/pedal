3e2b2bcbad305680c230731955da16156b328503
rujack <ffggabc123@gmail.com>
2021-10-18 08:24:19 +0700

    Pedal

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+const debug = process.env.NODE_ENV !== 'production'
+
 module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['1.bp.blogspot.com','2.bp.blogspot.com','3.bp.blogspot.com','4.bp.blogspot.com','i.ibb.co','i0.wp.com','i1.wp.com','i2.wp.com',''],
-  }
+  },
+  assetPrefix: !debug ? '/pedal/' : '',
 }
